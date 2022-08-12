c1e5a336fc3980194b71e718278289b6dcf66431
Kaique José de Souza Santos <kaique30122004@gmail.com>
2022-08-12 09:38:21 -0300

    First Commit

---
+++ _css/_js/js.js
@@ -0,0 +1,8 @@
+var visor = document.getElementById('visor').value;
+alert(visor);
+
+function somar() {}
+function subtrair() {}
+function multiplicar() {}
+function dividir() {}
+function mostrarResultado(){}
